8ff796fe19cddb58027339d306adb497499bb939
jsangmin99 <jsangmin99@gmail.com>
2024-05-29 15:16:48 +0900

    clear

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -14,5 +14,20 @@ git commit -m "메세지타이틀" -m "메세지 내용"
 # vi 모드에서 작성 : git commit 후 엔터 
 git commit
 
+# add 와 commit 동시에
+git commit -am "add와 커밋 동시에"
+
+
 # commit이력 확인 (local repository의 이력)
 git log
+
+#git log 간결하게
+git log --oneline
+
+# 전체 이력 조
+git log --all
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 출돌 무시하고 강제 push
